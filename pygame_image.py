a76302e9e6e1afa4eba709121f4b3bed0486d479
C0A22101 <c0a2210198@edu.teu.ac.jp>
2023-11-21 15:47:33 +0900

    練習5

--- pygame_image.py
+++ pygame_image.py
@@ -8,14 +8,15 @@ def main():
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")#練習１
     kk_img = pg.image.load("ex01/fig/3.png")#練習２
-    kk_img = pg.image.flip(kk_img, True, False)
-    kk_img = pg.image.rotozoom(kk_img, 10, 1.0)
+    kk_img = pg.transform.flip(kk_img, True, False)
+    kk_imgs = pg.transform.rotozoom(kk_img, 10, 1.0)
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0])#練習４
+        screen.blit(kk_imgs,[tmr%2], [300, 200])
         pg.display.update()
         tmr += 1        
         clock.tick(10)
